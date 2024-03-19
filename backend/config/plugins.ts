4e605f7686fbce7d418ecc30910d7620b6ff0fc0
Chiuchiu12138 <a1112chaohao@gmail.com>
2024-03-19 09:23:45 -0800

    Add backend

---
+++ backend/config/plugins.ts
@@ -0,0 +1,21 @@
+export default ({ env }) => {
+  return {
+    "schemas-to-ts": {
+      enabled: true,
+    },
+    email: {
+      config: {
+        provider: "amazon-ses",
+        providerOptions: {
+          key: env("AWS_SES_KEY"),
+          secret: env("AWS_SES_SECRET"),
+          amazon: "https://email.us-east-1.amazonaws.com",
+        },
+        settings: {
+          defaultFrom: "",
+          defaultReplyTo: "",
+        },
+      },
+    },
+  };
+};
