0a7426ecdf275b0f0fb70b361e2e1539c6274de6
Chiuchiu12138 <a1112chaohao@gmail.com>
2024-04-04 14:30:30 -0700

    Fix deployment issues

--- accounting-system/src/app/api/route.tsx
+++ accounting-system/src/app/api/route.tsx
@@ -1,10 +1,9 @@
 import sqlite3 from "sqlite3";
-import { open, Database } from "sqlite";
-import { NextApiRequest, NextApiResponse } from "next";
+import { open } from "sqlite";
 
 let db: any | null = null;
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(request: Request) {
   // Check if the database instance has been initialized
   if (!db) {
     // If the database instance is not initialized, open the database connection
@@ -18,8 +17,5 @@ export async function GET(req: NextApiRequest, res: NextApiResponse) {
   const items = await db.all("SELECT * FROM configuration");
 
   // Return the items as a JSON response with status 200
-  return new Response(JSON.stringify(items[0]), {
-    headers: { "Content-Type": "application/json" },
-    status: 200,
-  });
+  return Response.json(items[0]);
 }
